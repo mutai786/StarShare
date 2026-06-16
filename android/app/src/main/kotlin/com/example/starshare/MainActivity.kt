96fda80d19cee01f6088ffc922d29a8de522adef
mutai786 <aronmutai786@gmail.com>
2026-06-16 10:53:22 +0300

    StarShare second version

--- android/app/src/main/kotlin/com/example/starshare/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.starshare
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/mutai/starshare/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mutai.starshare
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
